58b88ac4f53711a78cea0d8a6606b59cdbc10b43
ValberJunior <valberjr1992@gmail.com>
2021-10-23 09:13:54 -0300

    update

--- script/script.js
+++ script/script.js
@@ -1,20 +1,6 @@
-//jquery
-$('.owl-carousel').owlCarousel({
-    loop:true,
-    margin:10,
-    nav:false,
-    responsive:{
-        0:{
-            items:3
-        },
-        600:{
-            items:4
-        },
-        1000:{
-            items:6
-        }
-    }
-})
+
+//Aos
+AOS.init();
 
 
 //Events
